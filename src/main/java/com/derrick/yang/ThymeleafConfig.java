ad6d4c04895332f1d7e5d58af5701bf27f5d46ed
yanggy <yangguiyun250725@163.com>
2018-10-11 15:16:47 +0800

    i18n

---
+++ src/main/java/com/derrick/yang/ThymeleafConfig.java
@@ -0,0 +1,25 @@
+package com.derrick.yang;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+/**
+ * @author derrick.yang
+ * @Date 10/11/18 10:51
+ */
+
+@Configuration
+public class ThymeleafConfig extends WebMvcConfigurerAdapter {
+    private LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
--- src/main/java/com/derrick/yang/api/IndexController.java
+++ src/main/java/com/derrick/yang/api/IndexController.java
@@ -1,8 +1,13 @@
 package com.derrick.yang.api;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 /**
  * @author derrick.yang
@@ -14,6 +19,15 @@ public class IndexController {
 
     @RequestMapping("/")
     public String index(ModelMap map) {
+        map.put("title","thymeleaf learning");
+        map.put("name","中国");
         return "index";
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return sessionLocaleResolver;
     }
 }
